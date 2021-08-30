4f61e11c11f98fdccc6bd634ff2535631ee03d42
Chansik Yun <chansigi@gmail.com>
2021-08-30 16:52:05 +0900

    .

---
+++ Kotlin_210830/src/08_봉인클래스2.kt
@@ -0,0 +1,22 @@
+// 08_봉인클래스2.kt
+package ex8_2
+
+open class Expr
+class Num(val value: Int): Expr()
+class Sum(val left: Expr, val right: Expr): Expr()
+
+fun eval(e: Expr): Int {
+    return when(e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        else -> throw IllegalStateException("Unknown expression")
+    }
+}
+
+fun main() {
+    val left = Num(10)
+    val right = Sum(Num(20), Num(10))
+    val sum = Sum(left, right)
+
+    println(eval(sum))
+}
