2849ae6370c1cb3c6aceb91ef5262f73ffeaec9c
Chansik Yun <chansigi@gmail.com>
2021-08-30 09:56:13 +0900

    .

--- Kotlin_210830/src/02_기본문법.kt
+++ Kotlin_210830/src/02_기본문법.kt
@@ -1,6 +1,8 @@
 // 02_기본문법.kt
 package ex2
 
+import javasample.ex2.Sample
+
 // 자바는 디렉토리를 통해 패키지가 구성되어야 하지만,
 // 코틀린은 패키지를 그냥 사용할 수 있습니다.
 // => 제품 코드에서는 기존의 자바의 형식을 따르는 것이 권장됩니다.
@@ -20,6 +22,16 @@ fun add(a: Int, b: Int) : Int {
     return a + b
 }
 
+//   Java: void foo() {}
+// Kotlin: void -> Unit
+//    Unit - 결과가 존재하지 않음을 나타내는 값
+//  "순수 함수: 함수의 인자가 동일하면, 결과가 동일하다."
+fun foo() {
+    println("foo")
+}
+
 fun main() {
     println(add(10, 20))
+    // println(foo())
+    println(Sample.foo())
 }
---
+++ Kotlin_210830/src/javasample/ex2/Sample.java
@@ -0,0 +1,11 @@
+package javasample.ex2;
+
+public class Sample {
+    public static void foo() {
+        System.out.println("foo");
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(foo());
+    }
+}
