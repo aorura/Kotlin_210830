f6d23961f68c07e62ab717a6d3c9c92907a82453
Chansik Yun <chansigi@gmail.com>
2021-08-30 15:26:20 +0900

    .

--- Kotlin_210830/src/javasample/ex7/Sample.java
+++ Kotlin_210830/src/javasample/ex7/Sample.java
@@ -1,7 +1,104 @@
 package javasample.ex7;
 
-public class Sample {
-    public static void main(String[] args) {
-        
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+// 파일에 위해 저장하기 위한 인터페이스 - Serializable
+interface State extends Serializable {
+}
+
+interface View {
+    State getState();               // 현재 상태를 반환합니다.
+
+    void setState(State state);     // 현재 상태를 변경합니다.
+}
+
+class Button implements View {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    @Override
+    public String toString() {
+        return "Button{" +
+                "x=" + x +
+                ", y=" + y +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    // ...
+    // 저장하고 복원해야 하는 상태는 별도의 클래스를 통해 캡슐화합니다.
+    // => Memento Pattern
+    // : 객체를 상태를 저장하고 복원하는 패턴
+
+    // static class: Nested class
+    //        class: Inner class
+    // > 내부 클래스의 문제점
+    //  : 내부 클래스는 자신을 감싸는 외부 클래스의 객체의 참조를 암묵적으로 소유합니다.
+    //    - 메모리 누수 등의 다양한 문제의 원인이 됩니다.
+    //    - 의도한 경우가 아니면, 내부 클래스가 아닌 중첩 클래스를 통해 설계를 해야 합니다.
+    private static class ButtonState implements State {
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        // Button otherObj;
+
+        private ButtonState(int x, int y, int width, int height) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    @Override
+    public State getState() {
+        return new ButtonState(x, y, width, height);
+    }
+
+    @Override
+    public void setState(State state) {
+        ButtonState s = (ButtonState) state;
+        x = s.x;
+        y = s.y;
+        width = s.width;
+        height = s.height;
+    }
+
+    public void foo() {
+        x += 10;
+        y += 10;
+        width += 100;
+        height += 100;
     }
 }
+
+
+public class Sample {
+    public static void saveStateToFile(State state) throws Exception {
+        FileOutputStream fos = new FileOutputStream("button.dat");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(state);
+
+        fos.close();
+        oos.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 1. 저장
+        Button button = new Button();
+        button.foo();
+
+        State state = button.getState();
+        saveStateToFile(state);
+    }
+}
+
+
+
