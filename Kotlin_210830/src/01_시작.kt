4977e5e51b5d07355ac20cb3ad0383e05b74a71d
Chansik Yun <chansigi@gmail.com>
2021-08-30 09:05:57 +0900

    .

---
+++ Kotlin_210830/src/01_시작.kt
@@ -0,0 +1,19 @@
+// 01_시작.kt
+// 1. 코틀린 언어 특징
+//  - 간결함
+//   : 자바에서 발생하는 보일러플레이트(상용구)를 제거합니다.
+//  - 안전함
+//  - 상호운용성
+
+/*
+// Hello.java
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
+*/
+fun main() {
+    println("Hello, Kotlin")
+}
+
