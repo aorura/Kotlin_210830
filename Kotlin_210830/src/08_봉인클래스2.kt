75f204c6f4754a02cdde2aa74b259bcd52a7008b
Chansik Yun <chansigi@gmail.com>
2021-08-30 17:07:37 +0900

    .

--- Kotlin_210830/src/08_봉인클래스2.kt
+++ Kotlin_210830/src/08_봉인클래스2.kt
@@ -1,21 +1,35 @@
 // 08_봉인클래스2.kt
 package ex8_2
 
+// Android - Java 8 / Java 11(2021) / Java 16
+// Kotlin은 새로운 언어의 기능을 안드로이드에서 바로 사용할 수 있습니다.
+// => 1.5
+
 // open class Expr
+/*
 sealed class Expr
+sealed class Expr2
 
 class Num(val value: Int) : Expr()
 class Sum(val left: Expr, val right: Expr) : Expr()
 class Diff(val left: Expr, val right: Expr): Expr()
+*/
 
 // 1. Expr이 open이기 때문에, 누구나 새로운 클래스를 상속 받을 수 있습니다.
 //  => else 구문이 필요합니다.
 // 2. 새로운 클래스가 추가되어도, eval의 코드가 제대로 동작하기 위해서는
 //    반드시 수정이 필요합니다.
-
 // 봉인 클래스(sealed class)를 통해 위의 문제를 해결할 수 있습니다.
 //  : 봉인 클래스에 대한 하위 클래스를 같은 파일에서만 작성할 수 있습니다.
 //   > 컴파일러는 봉인 클래스에 대한 하위 클래스가 정확히 몇개 존재하는지, 어떤 타입인지 알 수 있습니다.
+
+// 3. 코틀린 1.4에서 봉인된 인터페이스의 문법이 추가되었습니다.
+sealed interface Expr
+sealed interface Expr2
+
+class Num(val value: Int) : Expr, Expr2
+class Sum(val left: Expr, val right: Expr) : Expr
+class Diff(val left: Expr, val right: Expr): Expr
 
 fun eval(e: Expr): Int {
     return when (e) {
