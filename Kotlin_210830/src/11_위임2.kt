cef841ff351e54ef677febb4d4f54176be26bb52
Chansik Yun <chansigi@gmail.com>
2021-08-31 13:58:04 +0900

    .

---
+++ Kotlin_210830/src/11_위임2.kt
@@ -0,0 +1,28 @@
+// 11_위임2.kt
+package ex11_2
+
+import kotlin.reflect.KProperty
+
+// 위임
+// - 클래스 위임
+// - 프로퍼티 위임
+//  : 접근자 메소드에 대한 동작을 다른 객체에게 위임한다.
+
+class SampleDelegate {
+    // Type 'SampleDelegate' has no method
+    // 'getValue(User, KProperty<*>)' and thus it cannot serve as a delegate
+    operator fun getValue(thisRef: User, property: KProperty<*>): String {
+        return "Tom"
+    }
+}
+
+class User {
+    val name: String by SampleDelegate()
+    // name에 대한 getter 호출을 SampleDelegate 객체가 처리한다.
+    // => 약속된 메소드를 호출하는 연산자 오버로딩으로 구현됩니다.
+}
+
+fun main() {
+    val user = User()
+    println(user.name)
+}
