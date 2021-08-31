632be2f1bfb018579e48ff6ab198f5fa3b27a66d
Chansik Yun <chansigi@gmail.com>
2021-08-31 11:02:48 +0900

    .

--- Kotlin_210830/src/10_object.kt
+++ Kotlin_210830/src/10_object.kt
@@ -1,7 +1,6 @@
 // 10_object.kt
 package ex10
 
-
 // object 용법 3가지
 // 1. object 선언 - Singleton Pattern
 //    : constructor를 제공할 수 없습니다.
@@ -16,7 +15,42 @@ object Cursor {
     }
 }
 
+/*
 fun main() {
+    // Cursor <- 객체
+    val c = Cursor
+
     println("Main start")
     Cursor.move()
 }
+*/
+
+// 활용
+data class Person(val name: String)
+
+// Comparator 인터페이스를 구현하는 객체를 제공해야 합니다.
+class NameComparator : Comparator<Person> {
+    override fun compare(o1: Person, o2: Person): Int {
+        return o1.name.compareTo(o2.name)
+    }
+}
+
+fun main() {
+    val people = listOf(
+        Person("Tom"),
+        Person("Bob"),
+        Person("Alice")
+    )
+
+    val result1 = people.sortedWith(NameComparator())
+    println(result1)
+
+}
+
+
+
+
+
+
+
+
