0455034f15c9e7226a93181804f8c4c640309502
Chansik Yun <chansigi@gmail.com>
2021-08-31 14:45:25 +0900

    .

--- Kotlin_210830/src/11_위임3.kt
+++ Kotlin_210830/src/11_위임3.kt
@@ -1,7 +1,6 @@
 // 11_위임3.kt
 package ex11_3
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.function.Predicate
 import kotlin.reflect.KProperty
 
@@ -17,22 +16,40 @@ class SampleDelegate(var value: String) {
 }
 */
 
+
 // Generic
 // 1) 값이 조건에 부합되어야만 변경되도록 하고 싶습니다.
-class SampleDelegate<T>(var value: T, private val predicate: Predicate<T>? = null) {
+
+// 2) 프로퍼티의 값의 변경을 알 수 있는 위임 객체
+interface ValueChanged<T> {
+    fun onValueChanged(old: T, new: T)
+}
+
+class SampleDelegate<T>(
+    var value: T,
+    private val predicate: Predicate<T>? = null,
+    private val onValueChanged: ValueChanged<T>? = null
+) {
     operator fun getValue(thisRef: User, property: KProperty<*>): T {
         return value
     }
 
+    // 언어적으로 어떤 문법을 사용했을 때, 약속된 메소드가 호출됩니다.
+    // => 연산자 오버로딩
     operator fun setValue(thisRef: User, property: KProperty<*>, value: T) {
         // ?: - Elvis Operator
         //    - null일때 기본식(Expression)을 지정할 수 있습니다.
         val test: Boolean = predicate?.test(value) ?: true
         if (test) {
+            val old = this.value
+            val new = value
+
             this.value = value
+            onValueChanged?.onValueChanged(old, new)
         }
     }
 }
+
 
 /*
 interface Predicate<T> {
@@ -47,7 +64,20 @@ class IsValidName : Predicate<String> {
 }
 
 class User {
-    var name: String by SampleDelegate("", predicate = IsValidName())
+    operator fun plusAssign(i: Int) {
+        println("User에 ${i}를 더했습니다.")
+    }
+
+    var name: String by SampleDelegate(
+        "",
+        predicate = IsValidName(),
+        onValueChanged = object: ValueChanged<String> {
+            override fun onValueChanged(old: String, new: String) {
+                println("name: $old -> $new")
+            }
+        }
+    )
+
     var age: Int by SampleDelegate(0, predicate = object : Predicate<Int> {
         override fun test(t: Int): Boolean {
             return t > 0
@@ -57,6 +87,9 @@ class User {
 
 fun main() {
     val user = User()
+    user += 10
+
     user.name = "Bob"
+    user.name = "Tom"
     println(user.name)
 }
