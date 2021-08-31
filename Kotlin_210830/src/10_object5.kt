19a3ac865f15685a2a895d8199650895973d6a4d
Chansik Yun <chansigi@gmail.com>
2021-08-31 12:12:57 +0900

    .

---
+++ Kotlin_210830/src/10_object5.kt
@@ -0,0 +1,74 @@
+// 10_object5.kt
+// object
+// 1. object 선언
+// 2. companion object
+// 3. Anonymous object - 익명 객체
+
+interface MouseAdapter {
+    fun mouseClicked()
+    fun mouseEntered()
+}
+
+class Window {
+    var mouseAdapter: MouseAdapter? = null
+
+    fun click() {
+        // mouseAdapter.mouseClicked()
+        // Nullable 타입은 반드시 null check가 필요합니다.
+
+        // if (mouseAdapter != null) {
+        // mouseAdapter.mouseClicked()
+        // 스마트 캐스트가 동작하지 않습니다.
+        // : Smart cast to 'MouseAdapter' is impossible,
+        // because 'mouseAdapter' is a mutable property
+        //   that could have been changed by this time
+        // }
+
+        // 1) 강제 접근 - !!
+        // mouseAdapter!!.mouseClicked()
+        // : Null일 경우 예외가 발생합니다.
+
+        // 2) 지역 변수
+        // val mouseAdapter = mouseAdapter
+        // if (mouseAdapter != null) {
+        //    mouseAdapter.mouseClicked()
+        // }
+
+        // 3) Safe-Call
+        // 위의 코드를 간결하게 작성할 수 있는 문법이 있습니다.
+        mouseAdapter?.mouseClicked()
+    }
+
+    fun enter() {
+        mouseAdapter?.mouseEntered()
+    }
+}
+
+fun main() {
+    val window = Window()
+
+    // 익명의 객체를 생성할 때 object 키워드를 통해 생성할 수 있습니다.
+    window.mouseAdapter = object : MouseAdapter {
+        override fun mouseClicked() {
+            println("mouseClicked")
+        }
+
+        override fun mouseEntered() {
+            println("mouseEntered")
+        }
+    }
+
+    window.click()
+    window.click()
+    window.enter()
+    window.enter()
+}
+
+
+
+
+
+
+
+
+
