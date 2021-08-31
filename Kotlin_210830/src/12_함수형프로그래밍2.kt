1aaf06f9000763961c585b3a82338b09d1eaf9c0
Chansik Yun <chansigi@gmail.com>
2021-08-31 16:04:27 +0900

    .

--- Kotlin_210830/src/12_함수형프로그래밍2.kt
+++ Kotlin_210830/src/12_함수형프로그래밍2.kt
@@ -10,19 +10,37 @@ class Car {
     fun move(destination: String) {
         println("move - $destination")
     }
+
+    // 동반 객체에 이름을 부여하는 것이 가능합니다.
+    //  > 자바 쪽에서 참조할 때 사용되는 이름입니다.
+    companion object Companion {
+        // (Int, Int) -> Int
+        fun add(a: Int, b: Int): Int = a + b
+    }
 }
 
 // 메소드의 시그니처는 첫번째 인자로 객체에 대한 전달이 있습니다. - this
 fun main() {
+    // 동반 객체의 메소드를 참조할 때는, Car.Companion::add / (Car)::add
+    val fn5 = Car.Companion::add
+
+
     val fn1: (Car) -> Unit = Car::go
     val fn2: (Car, String) -> Unit = Car::move
 
     val car1 = Car()
     val car2 = Car()
 
+    // Bound Reference
+    val fn3: () -> Unit = car1::go
+    fn3()        // go(car1)
+    val fn4: (String) -> Unit = car1::move
+    fn4("Seoul") // move(car1, "Seoul")
+
     car1.go()   // go(car1)
     car2.go()   // go(car2)
 
     fn1(car1)
     fn1(car2)
+    fn2(car1, "Seoul")
 }
