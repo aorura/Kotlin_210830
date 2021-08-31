af4060b9c4cb7aa006e5a8040db43275c7edeebe
Chansik Yun <chansigi@gmail.com>
2021-08-31 15:00:48 +0900

    .

--- Kotlin_210830/src/11_위임4.kt
+++ Kotlin_210830/src/11_위임4.kt
@@ -22,21 +22,34 @@ class Image {
 //   => 지연 초기화
 //      객체를 바로 생성하는 것이 아니라, 처음으로 접근되는 시점에 생성한다.
 //      "스레드 안전합니다."
+//   => getter 에 대해서만 지원합니다.
+//      val 에서만 사용할 수 있습니다.
 class User {
     // val image: Image = Image()
     val image: Image by lazy {
         Image()
     }
     // 블록의 결과는 마지막 표현식에 의해 결정됩니다.
+    // var image2: Image? = null
+    lateinit var image2: Image
 
+    // * lateinit var
+    //  : 주의해서 사용해야 합니다.
+    // - 초기화되지 않은 프로퍼티를 사용할 경우, 예외가 발생합니다.
+    //   UninitializedPropertyAccessException
+    // - 참조 타입에 대해서만 사용 가능합니다.
+    //   : Int, Double, Long, Float ...
     fun drawImage() {
         image.draw()
+        image2.draw()
     }
 }
 
 fun main() {
     println("User 객체 생성 시작")
     val user = User()
+    user.image2 = Image()
+
     println("User 객체 생성 완료")
     user.drawImage() // !!!!!!
     user.drawImage()
