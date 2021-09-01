23c9f3bf83e0c5403664022788aad9d78a0acc50
Chansik Yun <chansigi@gmail.com>
2021-09-01 09:48:45 +0900

    .

--- Kotlin_210830/src/16_고차함수.kt
+++ Kotlin_210830/src/16_고차함수.kt
@@ -13,6 +13,11 @@ package ex16
 //   있습니다.
 
 
+// 변하지 않는 전체 알고리즘에서 변해야 하는 정책을 분리해야 합니다.
+//  "공통성과 가변성의 분리"
+//  - 함수에서 정책을 분리하는 방법
+//    1) 인터페이스 기반 정책 분리 방법
+//    2) 함수 기반 정책 분리 방법
 fun filterOdd(data: List<Int>): List<Int> {
     // val result: List<Int> = emptyList()
     val result: MutableList<Int> = mutableListOf()
@@ -26,9 +31,21 @@ fun filterOdd(data: List<Int>): List<Int> {
     return result
 }
 
+fun filterEven(data: List<Int>): List<Int> {
+    val result: MutableList<Int> = mutableListOf()
+
+    for (e in data) {
+        if (e % 2 == 0) {
+            result.add(e)
+        }
+    }
+
+    return result
+}
+
 fun main() {
     val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val result = filterOdd(list)
+    val result = filterEven(list)
 
     println(result)
 }
---
+++ Kotlin_210830/src/16_고차함수2.kt
@@ -0,0 +1,36 @@
+// 16_고차함수2.kt
+package ex16_2
+
+// 변해야 하는 정책을 인터페이스 기반으로 분리합니다.
+interface Predicate<E> {
+    fun test(e: E): Boolean
+}
+
+// 동작 파라미터화 설계 - 정책을 인터페이스 기반의 인자를 통해 전달합니다.
+fun filter(data: List<Int>, predicate: Predicate<Int>): List<Int> {
+    val result: MutableList<Int> = mutableListOf()
+
+    for (e in data) {
+        if (predicate.test(e)) {
+            result.add(e)
+        }
+    }
+
+    return result
+}
+
+// Predicate 인터페이스를 구현하는 다양한 필터의 정책 클래스를 제공합니다.
+class EvenPredicate : Predicate<Int> {
+    override fun test(e: Int): Boolean = e % 2 == 0
+}
+class OddPredicate : Predicate<Int> {
+    override fun test(e: Int): Boolean = e % 2 == 1
+}
+
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    var result = filter(list, EvenPredicate())
+    result = filter(list, OddPredicate())
+
+    println(result)
+}
