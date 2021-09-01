8acab690e2cb7d18665398787840b11025ab0c13
Chansik Yun <chansigi@gmail.com>
2021-09-01 10:37:59 +0900

    .

--- Kotlin_210830/src/11_위임5.kt
+++ Kotlin_210830/src/11_위임5.kt
@@ -3,16 +3,29 @@ package ex11_5
 
 import kotlin.reflect.KProperty
 
-class SampleDelegate(var value: String) {
-    operator fun getValue(thisRef: User, property: KProperty<*>): String {
+class SampleDelegate<T>(
+    var value: T,
+    val onValueChanged: (old: T, new: T) -> Unit
+    ) {
+    operator fun getValue(thisRef: User, property: KProperty<*>): T {
         return value
     }
 
-    operator fun setValue(thisRef: User, property: KProperty<*>, value: String) {
+    operator fun setValue(thisRef: User, property: KProperty<*>, value: T) {
+        val old = this.value
         this.value = value
+
+        onValueChanged(old, value)
     }
 }
 
 class User {
+    var name: String by SampleDelegate("Tom") { old, new ->
+        println("$old -> $new")
+    }
+}
 
+fun main() {
+    val user = User()
+    user.name = "Bob"
 }
