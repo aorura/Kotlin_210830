efb17ce2f01dce40131b9be6d3d4b10880d68aec
Chansik Yun <chansigi@gmail.com>
2021-09-01 14:24:09 +0900

    .

--- Kotlin_210830/src/19_인라인.kt
+++ Kotlin_210830/src/19_인라인.kt
@@ -1,6 +1,13 @@
 // 19_인라인.kt
 package ex19
 
+import java.lang.Exception
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+// 코틀린은 자바에서 제공하는 synchronized 문법이 제공되지 않습니다.
+/*
 class IncThread : Thread() {
     companion object {
         var n = 0
@@ -13,10 +20,90 @@ class IncThread : Thread() {
         }
     }
 }
+*/
+
+// Expected performance impact from inlining is insignificant.
+// Inlining works best for functions with parameters of functional types
+fun add(a: Int, b: Int) = a + b
+// 위의 함수에 대한 인라인에 대한 판단은 이미 JVM 상에서 효과적으로 수행됩니다.
+
+
+
+class IncThread(val lock: Lock) : Thread() {
+    companion object {
+        var n = 0
+    }
+
+    override fun run() {
+        for (i in 1..1_000_000) {
+
+//            withLock(lock) {
+//                n += 1
+//            }
+            lock.withLock {
+                n += 1
+            }
+
+
+        }
+    }
+
+    // lock.lock() / lock.unlock() 직접 호출할 경우
+    // 임계 영역 안에서 예외가 발생하게 되면, unlock이 호출되지 않아서,
+    // 데드락의 위험성이 있습니다.
+    /*
+    override fun run() {
+        for (i in 1..1_000_000) {
+            lock.lock()
+            //---
+            try {
+                n += 1
+            } catch (e: Exception) {
+            } finally {
+                lock.unlock()
+            }
+        }
+    }
+    */
+    /*
+    override fun run() {
+        for (i in 1..1_000_000) {
+            lock.lock()
+            //---
+            n += 1
+            //---
+            lock.unlock()
+        }
+    }
+    */
+}
+
+// inline 문법
+// => 함수를 호출하는 것이 아니라, 함수를 바이트코드로 치환하는 문법
+//   inline 키워드
+//    - 함수가 인자로 함수를 전달 받을 때에만 사용할 수 있습니다.
+// inline fun withLock(lock: Lock, block: () -> Unit) {
+inline fun<T> Lock.withLock2(block: () -> T): T {
+    lock()
+    try {
+        return block()
+    } finally {
+        unlock()
+    }
+}
 
 fun main() {
-    val t1 = IncThread()
-    val t2 = IncThread()
+    val lock = ReentrantLock()
+
+    val result = lock.withLock2 {
+        42
+    }
+    println(result)
+
+
+
+    val t1 = IncThread(lock)
+    val t2 = IncThread(lock)
 
     t1.start()
     t2.start()
