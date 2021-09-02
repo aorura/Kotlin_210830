d0429ed2402aa90abc3c68a3dc77a8d6bb167d8f
Chansik Yun <chansigi@gmail.com>
2021-09-02 11:41:05 +0900

    .

---
+++ Kotlin_210830/src/24_범위지정함수2.kt
@@ -0,0 +1,39 @@
+// 24_범위지정함수2.kt
+package ex24_2
+
+import ex24.apply
+
+data class User(val name: String, val age: Int)
+class SQLBuilder {
+    fun append(query: String) {}
+}
+
+class Database {
+    fun create(query: SQLBuilder): Boolean {
+        return true
+    }
+}
+
+fun main() {
+    val user = User("Tom", 42)
+    //----
+    val database = Database()
+    val query = SQLBuilder()
+    query.append("INSERT INTO user(name, age) VALUES ")
+    query.append("(${user.name}, ${user.age})")
+    val result = database.create(query)
+    println(result)
+    //----
+
+    val result2 = SQLBuilder()
+        .apply {
+            query.append("INSERT INTO user(name, age) VALUES ")
+            query.append("(${user.name}, ${user.age})")
+        }.also {
+            println("Query logging...")
+        }.run {
+            val database = Database()
+            database.create(this)
+        }
+    println(result2)
+}
